1a53797a196051148222803c6705bc1065c9ec68
unknown <raulsdev@gmail.com>
2020-09-02 14:35:38 -0500

    added heroku url to fetch

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-     fetch('http://localhost:3000/imageurl', {
+     fetch('https://limitless-springs-21760.herokuapp.com/imageurl', {
            method: 'post',
            headers: {
              'Content-Type': 'application/json'
@@ -95,7 +95,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://limitless-springs-21760.herokuapp.com:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
